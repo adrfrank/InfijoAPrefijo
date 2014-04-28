3154dddbb261f96ff3ab566dd63fc8e4c87414ef
Adrian Francisco Gonzalez Gutierrez <guanoman_adrian@hotmail.com>
2014-04-28 01:18:40 -0500

    First commit

---
+++ InfijoAPrefijo/InfijoAPrefijo.Form/ViewModels/InfixConverterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfijoAPrefijo.Form.ViewModels
+{
+    public class InfixConverterViewModel
+    {
+        string infix;
+
+        public string Infix
+        {
+            get { return infix; }
+            set { infix = value; }
+        }
+        string prefix;
+
+        public string Prefix
+        {
+            get { return prefix; }
+            set { prefix = value; }
+        }
+    }
+}
